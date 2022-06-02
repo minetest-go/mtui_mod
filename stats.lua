ef8068ba0dc8050614fa0a102d7e3e58a86d5a9f
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-06-02 17:44:27 +0200

    player_count

--- stats.lua
+++ stats.lua
@@ -1,20 +1,14 @@
 
 local function post_stats()
-    local players = {}
-    for _, player in ipairs(minetest.get_connected_players()) do
-        players[player:get_player_name()] = {}
-    end
-
     mtui.send_command({
         type = "stats",
         data = {
-            players = players,
+            player_count = #minetest.get_connected_players(),
             uptime = minetest.get_server_uptime(),
             max_lag = minetest.get_server_max_lag(),
             time_of_day = minetest.get_timeofday()
         }
     })
-
 
     minetest.after(2, post_stats)
 end
