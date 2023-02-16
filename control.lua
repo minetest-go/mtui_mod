07dfc21b732368a7414456774f7cead3b9dca0ad
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-02-16 13:28:16 +0100

    control registration actions

--- control.lua
+++ control.lua
@@ -13,6 +13,33 @@ function mtui.get_controls()
     return controls
 end
 
+-- returns only the controls metadata without action fields
+function mtui.get_controls_metadata()
+    local md = {}
+    for name, def in pairs(controls) do
+        md[name] = table.copy(def)
+        md[name].action = nil
+    end
+    return md
+end
+
+function mtui.get_controls_values()
+    local values = {}
+    for name, def in pairs(controls) do
+        if def.action and type(def.action.get) == "function" then
+            values[name] = def.action.get()
+        end
+    end
+    return values
+end
+
+function mtui.set_control_value(name, value)
+    local def = controls[name]
+    if def and def.action and type(def.action.set) == "function" then
+        def.action.set(value)
+    end
+end
+
 function mtui.get_control(name)
     return controls[name]
 end
--- control_registrations.lua
+++ control_registrations.lua
@@ -2,9 +2,11 @@
 mtui.register_control("mtui:shutdown", {
     type = "button",
     label = "Shutdown server",
-    set = function()
-        minetest.request_shutdown("planned shutdown")
-    end
+    action = {
+        set = function()
+            minetest.request_shutdown("planned shutdown")
+        end
+    }
 })
 
 mtui.register_control("mtui:tod", {
@@ -12,11 +14,13 @@ mtui.register_control("mtui:tod", {
     min = 0,
     max = 24,
     label = "Time of day (hour)",
-    get = function()
-        -- 1/10 decimal precision
-        return math.floor( minetest.get_timeofday() * 24 * 10 ) / 10
-    end,
-    set = function(n)
-        minetest.set_timeofday(n / 24)
-    end
+    action = {
+        get = function()
+            -- 1/10 decimal precision
+            return math.floor( minetest.get_timeofday() * 24 * 10 ) / 10
+        end,
+        set = function(n)
+            minetest.set_timeofday(n / 24)
+        end
+    }
 })
