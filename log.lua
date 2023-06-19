2694fda651c0f3ef7f3db28da22ae23ca47e9615
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-06-19 08:00:11 +0200

    add `mtui.log` / refactor logging

--- log.lua
+++ log.lua
@@ -1,135 +1,113 @@
 
-minetest.register_on_prejoinplayer(function(name, ip)
+function mtui.log(data)
+    data.category = "minetest"
+
     mtui.send_command({
         type = "log",
-        data = {
-            category = "minetest",
-            event = "prejoin",
-            username = name,
-            message = "'" .. name .. "' prejoins from '" .. ip .. "'",
-            ip_address = mtui.sanitize_ip(ip)
-        }
+        data = data
+    })
+end
+
+minetest.register_on_prejoinplayer(function(name, ip)
+    mtui.log({
+        event = "prejoin",
+        username = name,
+        message = "'" .. name .. "' prejoins from '" .. ip .. "'",
+        ip_address = mtui.sanitize_ip(ip)
     })
 end)
 
 minetest.register_on_joinplayer(function(player, last_login)
     local name = player:get_player_name()
     local pos = player:get_pos()
-    mtui.send_command({
-        type = "log",
-        data = {
-            category = "minetest",
-            event = "join",
-            username = name,
-            message = "'" .. name .. "' joins" .. (last_login and "" or " (new)"),
-            posx = pos.x,
-            posy = pos.y,
-            posz = pos.z
-        }
+
+    mtui.log({
+        event = "join",
+        username = name,
+        message = "'" .. name .. "' joins" .. (last_login and "" or " (new)"),
+        posx = pos.x,
+        posy = pos.y,
+        posz = pos.z
     })
 end)
 
 minetest.register_on_leaveplayer(function(player, timed_out)
     local name = player:get_player_name()
     local pos = player:get_pos()
-    mtui.send_command({
-        type = "log",
-        data = {
-            category = "minetest",
-            event = "leave",
-            username = name,
-            message = "'" .. name .. "' left" .. (timed_out and " (timed out)" or ""),
-            posx = pos.x,
-            posy = pos.y,
-            posz = pos.z
-        }
+
+    mtui.log({
+        event = "leave",
+        username = name,
+        message = "'" .. name .. "' left" .. (timed_out and " (timed out)" or ""),
+        posx = pos.x,
+        posy = pos.y,
+        posz = pos.z
     })
 end)
 
 minetest.register_on_authplayer(function(name, ip, is_success)
-    mtui.send_command({
-        type = "log",
-        data = {
-            category = "minetest",
-            event = "authplayer",
-            username = name,
-            message = "'" .. name .. "' authenticates from '" .. ip .. "' " ..
-                (is_success and "(success)" or "(failed)"),
-            ip_address = mtui.sanitize_ip(ip)
-        }
+    mtui.log({
+        event = "authplayer",
+        username = name,
+        message = "'" .. name .. "' authenticates from '" .. ip .. "' " ..
+            (is_success and "(success)" or "(failed)"),
+        ip_address = mtui.sanitize_ip(ip)
     })
 end)
 
 minetest.register_on_dieplayer(function(player, reason)
     local name = player:get_player_name()
     local pos = player:get_pos()
-    mtui.send_command({
-        type = "log",
-        data = {
-            category = "minetest",
-            event = "dieplayer",
-            username = name,
-            message = "'" .. name .. "' dies: " .. (reason and reason.type or "<no reason>"),
-            posx = pos.x,
-            posy = pos.y,
-            posz = pos.z
-        }
+
+    mtui.log({
+        event = "dieplayer",
+        username = name,
+        message = "'" .. name .. "' dies: " .. (reason and reason.type or "<no reason>"),
+        posx = pos.x,
+        posy = pos.y,
+        posz = pos.z
     })
 end)
 
 minetest.register_on_cheat(function(player, cheat)
     local name = player:get_player_name()
     local pos = player:get_pos()
-    mtui.send_command({
-        type = "log",
-        data = {
-            category = "minetest",
-            event = "cheat",
-            username = name,
-            message = "'" .. name .. "' cheats: " .. (cheat and cheat.type or "<unknown>"),
-            posx = pos.x,
-            posy = pos.y,
-            posz = pos.z
-        }
+
+    mtui.log({
+        event = "cheat",
+        username = name,
+        message = "'" .. name .. "' cheats: " .. (cheat and cheat.type or "<unknown>"),
+        posx = pos.x,
+        posy = pos.y,
+        posz = pos.z
     })
 end)
 
 minetest.register_on_chat_message(function(name, message)
     local player = minetest.get_player_by_name(name)
     local pos = player:get_pos()
-    mtui.send_command({
-        type = "log",
-        data = {
-            category = "minetest",
-            event = "chat",
-            username = name,
-            message = "'" .. name .. "' writes: '" .. message .. "'",
-            posx = pos.x,
-            posy = pos.y,
-            posz = pos.z
-        }
+
+    mtui.log({
+        event = "chat",
+        username = name,
+        message = "'" .. name .. "' writes: '" .. message .. "'",
+        posx = pos.x,
+        posy = pos.y,
+        posz = pos.z
     })
 end)
 
 minetest.register_on_mods_loaded(function()
-    mtui.send_command({
-        type = "log",
-        data = {
-            category = "minetest",
-            event = "system",
-            message = "started"
-        }
+    mtui.log({
+        event = "system",
+        message = "started"
     })
 end)
 
 minetest.register_on_shutdown(function()
-    mtui.send_command({
-        type = "log",
-        data = {
-            category = "minetest",
-            event = "system",
-            message = "shutdown"
-        }
+    mtui.log({
+        event = "system",
+        message = "shutdown"
     })
 end)
 
@@ -148,18 +126,14 @@ minetest.register_on_generated(function(minp, maxp)
         end
     end
 
-    mtui.send_command({
-        type = "log",
-        data = {
-            category = "minetest",
-            event = "on_generated",
-            username = playername,
-            message = "map generated between " ..
-                minetest.pos_to_string(minp) .. " and " .. minetest.pos_to_string(maxp),
-            posx = minp.x,
-            posy = minp.y,
-            posz = minp.z
-        }
+    mtui.log({
+        event = "on_generated",
+        username = playername,
+        message = "map generated between " ..
+            minetest.pos_to_string(minp) .. " and " .. minetest.pos_to_string(maxp),
+        posx = minp.x,
+        posy = minp.y,
+        posz = minp.z
     })
 end)
 
@@ -167,17 +141,14 @@ minetest.register_on_protection_violation(function(pos, name)
     if not name or name == "" then
         return
     end
-    mtui.send_command({
-        type = "log",
-        data = {
-            category = "minetest",
-            event = "protection_violation",
-            username = name,
-            message = "'" .. name .. "' violated a protection",
-            posx = pos.x,
-            posy = pos.y,
-            posz = pos.z
-        }
+
+    mtui.log({
+        event = "protection_violation",
+        username = name,
+        message = "'" .. name .. "' violated a protection",
+        posx = pos.x,
+        posy = pos.y,
+        posz = pos.z
     })
 end)
 
@@ -186,18 +157,15 @@ minetest.register_on_placenode(function(pos, newnode, placer)
         return
     end
     local name = placer:get_player_name()
-    mtui.send_command({
-        type = "log",
-        data = {
-            category = "minetest",
-            event = "placenode",
-            username = name,
-            nodename = newnode.name,
-            message = name .. " places node " .. newnode.name .. " at " .. minetest.pos_to_string(pos),
-            posx = pos.x,
-            posy = pos.y,
-            posz = pos.z
-        }
+
+    mtui.log({
+        event = "placenode",
+        username = name,
+        nodename = newnode.name,
+        message = name .. " places node " .. newnode.name .. " at " .. minetest.pos_to_string(pos),
+        posx = pos.x,
+        posy = pos.y,
+        posz = pos.z
     })
 end)
 
@@ -206,18 +174,15 @@ minetest.register_on_dignode(function(pos, oldnode, digger)
         return
     end
     local name = digger:get_player_name()
-    mtui.send_command({
-        type = "log",
-        data = {
-            category = "minetest",
-            event = "dignode",
-            username = name,
-            nodename = oldnode.name,
-            message = name .. " digs node " .. oldnode.name .. " at " .. minetest.pos_to_string(pos),
-            posx = pos.x,
-            posy = pos.y,
-            posz = pos.z
-        }
+
+    mtui.log({
+        event = "dignode",
+        username = name,
+        nodename = oldnode.name,
+        message = name .. " digs node " .. oldnode.name .. " at " .. minetest.pos_to_string(pos),
+        posx = pos.x,
+        posy = pos.y,
+        posz = pos.z
     })
 end)
 
@@ -226,18 +191,15 @@ minetest.register_on_punchnode(function(pos, node, puncher)
         return
     end
     local name = puncher:get_player_name()
-    mtui.send_command({
-        type = "log",
-        data = {
-            category = "minetest",
-            event = "punchnode",
-            username = name,
-            nodename = node.name,
-            message = name .. " punches node " .. node.name .. " at " .. minetest.pos_to_string(pos),
-            posx = pos.x,
-            posy = pos.y,
-            posz = pos.z
-        }
+
+    mtui.log({
+        event = "punchnode",
+        username = name,
+        nodename = node.name,
+        message = name .. " punches node " .. node.name .. " at " .. minetest.pos_to_string(pos),
+        posx = pos.x,
+        posy = pos.y,
+        posz = pos.z
     })
 end)
 
@@ -247,17 +209,14 @@ minetest.register_on_craft(function(itemstack, player)
     end
     local name = player:get_player_name()
     local pos = player:get_pos()
-    mtui.send_command({
-        type = "log",
-        data = {
-            category = "minetest",
-            event = "craft",
-            username = name,
-            nodename = itemstack:get_name(),
-            message = name .. " crafts " .. itemstack:get_name(),
-            posx = pos.x,
-            posy = pos.y,
-            posz = pos.z
-        }
+
+    mtui.log({
+        event = "craft",
+        username = name,
+        nodename = itemstack:get_name(),
+        message = name .. " crafts " .. itemstack:get_name(),
+        posx = pos.x,
+        posy = pos.y,
+        posz = pos.z
     })
 end)
