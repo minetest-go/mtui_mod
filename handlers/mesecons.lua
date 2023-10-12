e44b675b4d33f837a755be6250d34bdef823c865
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-10-12 11:32:26 +0200

    set/get

--- handlers/mesecons.lua
+++ handlers/mesecons.lua
@@ -1,14 +1,32 @@
 
--- enable switch or press button for an amount of time
-mtui.register_on_command("mesecons_enable", function(data)
-    -- TODO
-end)
+local button_off_def = minetest.registered_nodes["mesecons_button:button_off"]
 
--- disable switch
-mtui.register_on_command("mesecons_disable", function(data)
-    -- TODO
-end)
+-- enable/disable switch or press button for an amount of time
+mtui.register_on_command("mesecons_set", function(data)
+    minetest.load_area(data.pos)
+    local node = minetest.get_node_or_nil(data.pos)
+    if node == nil then
+        -- not loaded
+        return
+    end
 
+    if data.state == "on" then
+        if node.name == "mesecons_switch:mesecon_switch_off" then
+            -- switch in off-state, turn on
+            mesecon.receptor_on(data.pos)
+        elseif node.name == "mesecons_button:button_off" and button_off_def then
+            -- button in off-state, turn on for 1 second
+            button_off_def.on_rightclick(data.pos, node)
+            -- call turnoff async, in case the node-timer does not fire (unloaded area)
+            minetest.after(1, mesecon.button_turnoff, data.pos)
+        end
+    elseif data.state == "off" then
+        if node.name == "mesecons_switch:mesecon_switch" then
+            -- switch in on-state, turn off
+            mesecon.receptor_off(data.pos)
+        end
+    end
+end)
 
 -- catch on/off events from lightstones
 local lightstone_colors = {"red", "green", "blue", "gray", "darkgray",
@@ -19,7 +37,7 @@ for _, color in ipairs(lightstone_colors) do
         local old_action_on = def_on.mesecons.action_on
         def_on.mesecons.action_on = function(pos, node)
             mtui.send_command({
-                type = "mesecons_on",
+                type = "mesecons_event",
                 data = {
                     pos = pos,
                     state = "on",
@@ -36,7 +54,7 @@ for _, color in ipairs(lightstone_colors) do
         local old_action_off = def_off.mesecons.action_off
         def_off.mesecons.action_off = function(pos, node)
             mtui.send_command({
-                type = "mesecons_off",
+                type = "mesecons_event",
                 data = {
                     pos = pos,
                     state = "off",
