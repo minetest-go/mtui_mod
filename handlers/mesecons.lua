5439acdf481c8dbadff8c3e8ad350b5a05f6057c
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-10-12 12:54:29 +0200

    events

--- handlers/mesecons.lua
+++ handlers/mesecons.lua
@@ -1,6 +1,9 @@
 
-local button_off_def = minetest.registered_nodes["mesecons_button:button_off"]
+local button_off_def = assert(minetest.registered_nodes["mesecons_button:button_off"])
+local switch_off_def = assert(minetest.registered_nodes["mesecons_switch:mesecon_switch_off"])
+local switch_on_def = assert(minetest.registered_nodes["mesecons_switch:mesecon_switch_on"])
 
+-- ui -> game
 -- enable/disable switch or press button for an amount of time
 mtui.register_on_command("mesecons_set", function(data)
     minetest.load_area(data.pos)
@@ -13,20 +16,52 @@ mtui.register_on_command("mesecons_set", function(data)
     if data.state == "on" then
         if node.name == "mesecons_switch:mesecon_switch_off" then
             -- switch in off-state, turn on
-            mesecon.receptor_on(data.pos)
-        elseif node.name == "mesecons_button:button_off" and button_off_def then
+            switch_off_def.on_rightclick(data.pos, node)
+        elseif node.name == "mesecons_button:button_off" then
             -- button in off-state, turn on for 1 second
             button_off_def.on_rightclick(data.pos, node)
             -- call turnoff async, in case the node-timer does not fire (unloaded area)
             minetest.after(1, mesecon.button_turnoff, data.pos)
         end
     elseif data.state == "off" then
-        if node.name == "mesecons_switch:mesecon_switch" then
+        if node.name == "mesecons_switch:mesecon_switch_on" then
             -- switch in on-state, turn off
-            mesecon.receptor_off(data.pos)
+            switch_on_def.on_rightclick(data.pos, node)
         end
     end
 end)
+
+-- game -> ui
+-- catch switch states
+
+-- on -> off
+local old_switch_on_rightclick_action = switch_on_def.on_rightclick
+switch_on_def.on_rightclick = function(pos, node)
+    mtui.send_command({
+        type = "mesecons_event",
+        data = {
+            pos = pos,
+            state = "off",
+            name = node.name
+        }
+    })
+    return old_switch_on_rightclick_action(pos, node)
+end
+
+-- off -> on
+local old_switch_off_rightclick_action = switch_off_def.on_rightclick
+switch_off_def.on_rightclick = function(pos, node)
+    mtui.send_command({
+        type = "mesecons_event",
+        data = {
+            pos = pos,
+            state = "on",
+            name = node.name
+        }
+    })
+    return old_switch_off_rightclick_action(pos, node)
+end
+
 
 -- catch on/off events from lightstones
 local lightstone_colors = {"red", "green", "blue", "gray", "darkgray",
