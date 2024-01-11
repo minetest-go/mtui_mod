1c38bc3fda95725267664789b0fa169f84b2d5d7
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-01-11 13:41:49 +0100

    controls data types

--- handlers/controls.lua
+++ handlers/controls.lua
@@ -1,13 +1,12 @@
 
 mtui.register_on_command("get_controls_metadata", function()
-	return { success = true, result = mtui.get_controls_metadata() }
+	return mtui.get_controls_metadata()
 end)
 
 mtui.register_on_command("get_controls_values", function()
-	return { success = true, result = mtui.get_controls_values() }
+	return mtui.get_controls_values()
 end)
 
 mtui.register_on_command("set_control", function(data)
-    mtui.set_control_value(data.name, data.value)
-	return { success = true, result = mtui.get_controls_metadata() }
+    return mtui.set_control_value(data.name, data.value)
 end)
