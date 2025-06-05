902afbb6b2266846d5a5c90bc0d331a2c639a489
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2025-06-05 21:56:38 +0200

    log

--- prejoin_ratelimit.lua
+++ prejoin_ratelimit.lua
@@ -2,12 +2,19 @@
 -- ip -> os.time()
 local last_prejoin_time = {}
 
-minetest.register_on_prejoinplayer(function(_, ip)
+minetest.register_on_prejoinplayer(function(name, ip)
     local now = os.time()
     local last_time = last_prejoin_time[ip] or 0
 
     -- check for previous login attempt
     if now - last_time < 5 then
+        mtui.log({
+            event = "prejoin",
+            username = name,
+            message = "ratelimited prejoin for '" .. name .. "' with ip '" .. ip .. "'",
+            ip_address = mtui.sanitize_ip(ip)
+        })
+
         return "login attempts are rate-limited, try again later"
     end
 
